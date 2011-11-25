59568e1290f0c83a4de879c739f9fdf6b9d55057
pH <pedrohfranceschi@gmail.com>
2011-11-25 03:35:12 -0200

    [SERVER] Implemented create_group API method.

--- server/api.js
+++ server/api.js
@@ -1,16 +1,27 @@
 var database = require('./database.js'),
     sys = require('sys');
     
-exports._userInformationsResponseFromObject = function(user) {
+// Response helpers
+
+var userInformationsResponseFromObject = function(user) {
     return {
+        "id": user.id,
         "email": user.email,
         "name": user.name,
-        "id": user.id,
         "createdAt": user.createdAt
     }
 }
 
-exports._generateResponseString = function(success, message, otherFields) {
+var groupInformationsResponseFromObject = function(group) {
+    return {
+        "id": group.id,
+        "name": group.name,
+        "description": group.description,
+        "creatorId": group.creatorId
+    }
+}
+
+var generateResponseString = function(success, message, otherFields) {
     var responseHash =  {
         'success': success,
         'message': message,
@@ -22,38 +33,69 @@ exports._generateResponseString = function(success, message, otherFields) {
     
     return JSON.stringify(responseHash);
 }
+
+// API filters
+
+exports.verifyCredentialsFilter = function (req, res, next) {    
+    if(!req.param('email') || !req.param('password')) {
+        return res.send(generateResponseString(false, "Missing parameters.", {}));
+    } else {
+        database.users.find({ where: { email: req.param('email'), password: req.param('password') } }).on('success', function(user){
+            if(user) {
+                req['user'] = user;
+                next();
+            } else {
+                return res.send(generateResponseString(false, "Invalid email and/or password.", {}));
+            }
+        });
+    }
+};
+
+// API methods
     
 exports.verifyCredentials = function(req, res) {
     console.log(sys.inspect(req.params));
     if(!req.param('email') || !req.param('password')) {
-        return res.send(exports._generateResponseString(false, "Missing parameters.", {}));
+        return res.send(generateResponseString(false, "Missing parameters.", {}));
     } else {
         database.users.find({ where: { email: req.param('email'), password: req.param('password') } }).on('success', function(user){
             if(user) {
-                return res.send(exports._generateResponseString(true, null, { 'user': exports._userInformationsResponseFromObject(user) }));
+                return res.send(generateResponseString(true, null, { 'user': userInformationsResponseFromObject(user) }));
             } else {
-                return res.send(exports._generateResponseString(false, "Invalid email and/or password.", {}));
+                return res.send(generateResponseString(false, "Invalid email and/or password.", {}));
             }
         }).on('failure', function(error) {
-            return res.send(exports._generateResponseString(false, "Internal error (" + error.toString() + ").", {}));
+            return res.send(generateResponseString(false, "Internal error (" + error.toString() + ").", {}));
         });
     }
 };
 
 exports.createUser = function(req, res) {
     if(!req.param('email') || !req.param('password') || !req.param('name')) {
-        return res.send(exports._generateResponseString(false, "Missing parameters.", {}));
+        return res.send(generateResponseString(false, "Missing parameters.", {}));
     } else {
         database.users.find({ where: { email: req.param('email') } }).on('success', function(user){
             if(user) {
-                return res.send(exports._generateResponseString(false, "E-mail already registered.", {}));
+                return res.send(generateResponseString(false, "E-mail already registered.", {}));
             } else {
                 database.users.build({ name: req.param('name'), email: req.param('email'), password: req.param('password') }).save().on('success', function(user){
-                    return res.send(exports._generateResponseString(true, null, { 'user': exports._userInformationsResponseFromObject(user) }));
+                    return res.send(generateResponseString(true, null, { 'user': userInformationsResponseFromObject(user) }));
                 }).on('failure', function(error) {
-                    return res.send(exports._generateResponseString(false, "Internal error (" + error.toString() + ").", {}));
+                    return res.send(generateResponseString(false, "Internal error (" + error.toString() + ").", {}));
                 });
             }
+        });
+    }
+};
+
+exports.createGroup = function(req, res) {
+    if(!req.param('name')) { // email and password are already handled in filter
+        return res.send(generateResponseString(false, "Missing parameters.", {}));
+    } else {
+        database.groups.build({ name: req.param('name'), description: req.param('description'), creatorId: req.user.id }).save().on('success', function(group){
+            return res.send(generateResponseString(true, null, { 'group': groupInformationsResponseFromObject(group) }));
+        }).on('failure', function(error) {
+            return res.send(generateResponseString(false, "Internal error (" + error.toString() + ").", {}));
         });
     }
 };
--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 var express = require('express'),
-    sys = require('sys');
+    sys = require('sys'),
+    api = require('./api.js');
     
 var app = express.createServer();
+
+app.use(express.bodyParser());
 
 app.get('/', function(req, res){
     res.send('Hello World');
 });
+
+app.post('/api/verify_credentials', api.verifyCredentials);
+app.post('/api/create_user', api.createUser);
+app.post('/api/create_group', api.verifyCredentialsFilter, api.createGroup);
 
 app.listen(3000);
 console.log("Server on port %s", app.address().port);
